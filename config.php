d85388f0534401efe8cbe5f7d011d4f0d2e200c4
balochiyar00 <balochiyar00@gmail.com>
2025-08-12 12:09:21 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// توکن رباتت رو اینجا بذار
+define('BOT_TOKEN', '8471018926:AAEv7SfKS9Luxo9poCBf0VEdWsmcoSHWgRI');
+
+// آدرس پایگاه‌داده (در صورت نیاز)
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'music_store');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// آدرس دامنه‌ای که روی Render هستی
+define('BASE_URL', 'https://your-app-name.onrender.com');
+?>
